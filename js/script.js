a6e99b1b23d01626b7ffced3c82c7f2d96d03092
JuliaSaenko <julia.saenko94@gmail.com>
2018-09-12 21:31:14 +0300

    Слайдер на JS

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+/*--Форма--*/
 var button = document.querySelector(".popup-open");
 var popup = document.querySelector(".contact-us");
 var close = popup.querySelector(".form-close");
@@ -34,5 +35,33 @@ form.addEventListener("submit", function (evt) {
     popup.classList.remove("modal-error");
     popup.offsetWidth = popup.offsetWidth;
     popup.classList.add("popup-error");
-  }
+   }
 });
+
+/*--Форма--*/
+
+/*--Слайдер--*/
+
+var slider = document.querySelector(".slider");
+var slider_slides = slider.querySelectorAll(".slider-item");
+var slider_controls = slider.querySelectorAll(".slider-control");
+var slider_start = 0;
+
+for(var i = 0; i < slider_controls.length; i++) {
+  slider_controls[i].addEventListener("click", changeSlide.bind(slider_controls[i], i));
+}
+
+function changeSlide(id){
+  event.preventDefault();
+
+  for(var i = 0; i < slider_slides.length; i++) {
+    slider_slides[i].classList.remove("active-slide");
+    slider_controls[i].classList.remove("active-slide");
+  }
+
+  slider_slides[id].classList.add("active-slide");
+  slider_controls[id].classList.add("active-slide");
+
+  slider_start = id;
+}
+/*--Слайдер--*/
